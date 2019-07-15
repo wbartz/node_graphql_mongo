841f37188630b0378135743f1ffe1520939ae345
William Bartz <william.bartz@softdesign.com.br>
2019-07-15 07:56:07 -0300

    Initial commit

---
+++ src/main/kotlin/com/wbartz/sch_api/SchApiApplication.kt
@@ -0,0 +1,11 @@
+package com.wbartz.sch_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SchApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<SchApiApplication>(*args)
+}
