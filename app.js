fcab35c47cf3749ab6a624e424a77ac1f4c4aff3
William Bartz <william.bartz@softdesign.com.br>
2019-07-25 17:17:48 -0300

    Migração para node com graphQL

---
+++ app.js
@@ -0,0 +1,90 @@
+const express = require("express");
+const expressGraphQL = require("express-graphql");
+const statusMonitor = require("express-status-monitor");
+const helmet = require("helmet");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const {
+  GraphQLID,
+  GraphQLString,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLSchema
+} = require("graphql");
+
+const app = express();
+
+app.use(statusMonitor());
+app.use(helmet());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+mongoose.connect(
+  "mongodb+srv://william_bartz:f99tFsYEcpTZAA8o@cluster0-jdu4n.mongodb.net/calculo-horas?retryWrites=true&w=majority"
+);
+
+const UserModel = mongoose.model("usuarios", {
+  name: String,
+  email: String,
+  login: String
+});
+
+const UserType = new GraphQLObjectType({
+  name: "User",
+  fields: {
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    login: { type: GraphQLString }
+  }
+});
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: "Query",
+    fields: {
+      people: {
+        type: GraphQLList(UserType),
+        resolve: (root, args, context, info) => {
+          return UserModel.find().exec();
+        }
+      },
+      person: {
+        type: UserType,
+        args: {
+          id: { type: GraphQLNonNull(GraphQLID) }
+        },
+        resolve: (root, args, context, info) => {
+          return UserModel.findById(args.id).exec();
+        }
+      }
+    }
+  }),
+  mutation: new GraphQLObjectType({
+    name: "Mutation",
+    fields: {
+      person: {
+        type: UserType,
+        args: {
+          name: { type: GraphQLNonNull(GraphQLString) },
+          email: { type: GraphQLNonNull(GraphQLString) },
+          login: { type: GraphQLNonNull(GraphQLString) }
+        },
+        resolve: (root, args, context, info) => {
+          var person = new UserModel(args);
+          return person.save();
+        }
+      }
+    }
+  })
+});
+
+app.use(
+  "/graphql",
+  schema,
+);
+
+app.listen(3001, () => {
+  console.log(`Listening at 3001`);
+});
